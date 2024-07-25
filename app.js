53736de464f38308d42065bf1d227ab6dc152028
shivam-bhadani <shivambhadani123@gmail.com>
2024-07-25 23:16:05 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to AWS tutorial by Shivam Bhadani");
+})
+
+app.get("/health", (req, res) => {
+  res.send("Everything is OK");
+})
+
+app.listen(8080, () => console.log("Sever is listening on Port 8080"))
